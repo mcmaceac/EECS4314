07c84051a0e1c2993a5a4730d48121291593a0a9
mcmaceac <mcm.maceachern@gmail.com>
2017-11-19 11:54:20 -0500

    Added formatted Understand output for comparison

---
+++ A3/DependencyExtractor.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class DependencyExtractor {
+	
+	public static PrintWriter output;
+
+	static int totalFiles = 0;
+	
+	public static void main(String[] args) {
+		
+		File inFile = new File(args[0]);
+		File outFile = new File("./output.txt");
+		
+		try {
+			output = new PrintWriter(outFile);
+			output.println("Include File Dependency Extraction");
+			System.out.println("Starting dependency scan...");
+			
+			listFilesForFolder(inFile);
+			
+			output.close();
+
+			System.out.println("Dependency scan complete with " + totalFiles + " files scanned.");
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void listFilesForFolder(File folder) {
+		
+		List<String> list = new ArrayList<>();
+		
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				listFilesForFolder(fileEntry);
+			}
+			else {
+				
+				String fileName = fileEntry.getPath();
+				
+				if (fileName.endsWith(".cc") || fileName.endsWith(".cpp") || 
+					fileName.endsWith(".c")|| fileName.endsWith(".h") ||
+					fileName.endsWith(".java")) {
+				
+					totalFiles++;
+					
+					try (BufferedReader br = new BufferedReader(new FileReader(fileEntry))) {
+						String line;
+						
+						line = br.readLine();
+						while (line != null) {
+							if (line.startsWith("#include")) {
+								line = line.substring(10, line.length() - 1);
+								list.add(line.split("[\"> ]")[0]);
+							}
+							else if (line.startsWith("import")) {
+								list.add(line.substring(7, line.length() - 1));
+							}
+							line = br.readLine();
+						}
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					for (String l : list) {
+						String outString = fileEntry.getPath() + " -> " + l;
+						output.println(outString);
+					}
+					list.clear();
+				}
+			}
+		}
+	}
+}
